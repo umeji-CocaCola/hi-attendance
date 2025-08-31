f4fecb6097f1a5c9c646be1520b9fb4d18dc4264
umeji-CocaCola <stand.blue.2@gmail.com>
2025-08-31 11:22:06 +0900

    Prettierで改行と整形を統一しました

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 export default function Page() {
   const handleClick = (msg: string) => {
@@ -12,25 +12,25 @@ export default function Page() {
 
       <div className="flex gap-4 flex-wrap">
         <button
-          onClick={() => handleClick("出勤!")}
+          onClick={() => handleClick('出勤!')}
           className="px-4 py-2 rounded bg-green-500 text-white"
         >
           出勤
         </button>
         <button
-          onClick={() => handleClick("退勤!")}
+          onClick={() => handleClick('退勤!')}
           className="px-4 py-2 rounded bg-red-500 text-white"
         >
           退勤
         </button>
         <button
-          onClick={() => handleClick("休憩開始!")}
+          onClick={() => handleClick('休憩開始!')}
           className="px-4 py-2 rounded bg-yellow-500 text-black"
         >
           休憩開始
         </button>
         <button
-          onClick={() => handleClick("休憩終了!")}
+          onClick={() => handleClick('休憩終了!')}
           className="px-4 py-2 rounded bg-blue-500 text-white"
         >
           休憩終了
