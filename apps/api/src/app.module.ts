8919c948a30e7c28666e9f141b807db95f387b97
umeji-CocaCola <stand.blue.2@gmail.com>
2025-09-01 03:28:07 +0900

    feat(api): persist clock-in to Postgres via Prisma (#7)

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AttendanceController } from './attendance.controller';
+import { AttendanceService } from './attendance.service';
+import { PrismaModule } from './prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, AttendanceController], // ← ここに登録されていること
-  providers: [],
+  imports: [PrismaModule],
+  controllers: [AppController, AttendanceController],
+  providers: [AttendanceService],
 })
 export class AppModule {}
