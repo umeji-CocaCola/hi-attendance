1a07dc43db388925811808312272194275a60bdd
umeji-CocaCola <stand.blue.2@gmail.com>
2025-08-31 12:29:49 +0900

    feat(api): add /health endpoint,

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,12 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
-@Controller()
+@Controller('') // 空でルートにぶら下げる
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('health')
+  health() {
+    return { ok: true };
   }
 }
